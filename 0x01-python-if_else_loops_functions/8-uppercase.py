c44e76f41aab3a535a0d0a232ea2dad661d555fe
Benonking <benonkghoma@gmail.com>
2022-11-30 12:49:20 -0800

    is upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in range (ord('A'), ord('Z')):
-        if str = c:
-            return True
-        else:
-            return False
+    return (ord('A') <= ord(str) <= ord('Z'))

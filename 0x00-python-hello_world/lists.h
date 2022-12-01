ffcf71e6b6d47b9b74dedea45b60bc4e7d3f4590
Benonking <benonkghoma@gmail.com>
2022-11-30 22:23:29 -0800

    header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -24,5 +24,5 @@ int check_cycle(listint_t *list);
 size_t print_listint(const listin_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+int check_cycle(listint_t *list);
 #endif

cbe5c279392cd3c23a030ed2d6f511bfde8883e3
Benonking <benonkghoma@gmail.com>
2022-12-05 06:43:17 -0800

    make copy of list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     list_copy = my_list.copy()
     if idx >= 0 or idx < len(my_list):
         list_copy[idx] = element
-        return list_copy
+     return list_copy
 

1b7ad790217beaf9c62435f93dcd5d0263d17217
Benonking <benonkghoma@gmail.com>
2022-12-05 06:15:02 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string
-    print("{}".format(my_string) if i != 'c' or i == 'C')
+    return ("".join(i for i in my_string if c not in "Cc"))

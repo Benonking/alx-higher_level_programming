b5e8f20bac9d59d94af27e8663eff80e013ee505
Benonking <benonkghoma@gmail.com>
2022-12-04 20:12:06 -0800

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range (1, 100):
-        print("{} ".format(num))
-        if (num % 3 == 0):
-            num  = "Fizz"
-            print("{}".format(num))
-        if (num % 5 == 0):
-            num  = "Buzz"
-            print("{}".format(num))
+    for num in range (1, 101:
+        if num % 3 == 0:
+            print('Buzz', end='')
+        elif num % 5 == 0:
+            print('Buzz', end='')
+        elif num % 15 == 0:
+            print('FizzBuzz', end='')
+        else:
+            print ('{}'.format(i), end='')

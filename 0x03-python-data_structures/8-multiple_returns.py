58a12dafca839b509e7f6716e478dfd5192d3a90
Benonking <benonkghoma@gmail.com>
2022-12-05 18:59:31 -0800

    return tupple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) <= 0:
+        sentence[0] = None
+    else:
+        new_tuple = (len(setence), sentence[0])
+    return new_tuple
+

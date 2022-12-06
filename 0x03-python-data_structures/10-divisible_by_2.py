9d9a99e9a5428e4a5faa40a0bd1dcccf533688a7
Benonking <benonkghoma@gmail.com>
2022-12-06 01:33:46 -0800

    find even

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/pyhton3
 def divisible-by_2(my_list=[]):
     new_list = []
-    if len(my_list) < 1:
-        return None
-    else:
+    if my_list:
         for i in my_list:
-            new_list = new_list.append(if(i % 2 == 2))
+            if i % 2 == 0
+            new_list.append(True)
+        else:
+            new_list.append(False)
         return new_list

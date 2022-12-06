feff9f38a0cd693348265428f1d2766f8691be24
Benonking <benonkghoma@gmail.com>
2022-12-06 00:46:20 -0800

    add tupples

--- 0x03-python-data_structures/7-add_tupple.py
+++ 0x03-python-data_structures/7-add_tupple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def add_tuple(tupple_a(), tupple_b()):
-    for i in range (0, len(tupple_a)):
-        return (tupple_a[i] + tupple_b[i])
+def add_tuple(tupple_a = (), tupple_b = ()):
+    a =len(tuple_a)
+    b = len(tuple_b)
+    addition = (
+            (tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
+            (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0)
+            )
+    return addition

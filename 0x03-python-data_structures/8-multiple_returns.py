2ff7ba6e9c14fc6321f53f7dfb1b509564283f1c
Benonking <benonkghoma@gmail.com>
2022-12-05 19:05:23 -0800

    return tupple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     if len(sentence) <= 0:
         sentence[0] = None
     else:
-        Length, First character = len(setence), sentence[0]
-    return (Length, First character)
+        Length, char = len(setence), sentence[0]
+    return (Length, char)
 

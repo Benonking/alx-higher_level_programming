a880228db5cff45753d5870ba03f5aadf76842d7
Benonking <benonkghoma@gmail.com>
2022-12-06 01:18:53 -0800

    find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/pyhton3
 def max_integer(my_list = []):
-    if len(my_list) < 1:
-        return None
-    else:
+    if len(my_list) > 0:
         maxx = my_list[0]
         for i in my_list:
             if i > maxx:
-               maxx = i
+                maxx = i
         return maxx
+    else:
+        return None

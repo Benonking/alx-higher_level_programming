064826c305fae74c764d0af2ced4d6dc20e464ad
Benonking <benonkghoma@gmail.com>
2022-12-06 00:56:51 -0800

    more returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) <= 0:
         return None
     else:
-        Length, char = len(setence), sentence[0]
+        Length, char = len(sentence), sentence[0]
     return (Length, char)

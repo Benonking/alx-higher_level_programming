7bfd83e405c1a7e2e3db57a84430af29700b16c2
Benonking <benonkghoma@gmail.com>
2022-12-07 23:38:00 -0800

    command line ards

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
             print("{} arguments:".format(len(sys.argv)-1))
             for i in range(1, len(sys.argv)):
                 print("{}: {}".format(i, sys.argv[i]))
-            elif len(sys.argv) == 1:
+    elif len(sys.argv) == 1:
                 print('{} arguments.'.format(0))

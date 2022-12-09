072f54d7cc111defa0ee13aa685d1f540b077677
Benonking <benonkghoma@gmail.com>
2022-12-09 01:02:35 -0800

    square elements of list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix != None:
-        NewMatrix=[]
+        NewMatrix = []
         for row in matrix:
-            NewMatrix.append(list(map(lambda x: x**2,row)))
+            NewMatrix.append(list(map(lambda x: x ** 2, row)))
         return (NewMatrix)
     return None

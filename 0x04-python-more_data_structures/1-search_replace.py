b780a303c3a4b94bb018dc698e8deae66591c9b8
Benonking <benonkghoma@gmail.com>
2022-12-09 01:58:37 -0800

    search and replace element of list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,4 @@
 #!/usr/bin/pythom3
-'''
-my_list: is the initial list
-search : is the element to replace in the list
-replace : is the new element
-'''
-
-
 def search_replace(my_list, search, replace):
     if my_list is not None:
         return ([i if i != search else replace for i in my_list])

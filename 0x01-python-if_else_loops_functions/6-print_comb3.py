5ebf3ed69bf16e8c1008e49020434f5dc9aa0b22
Benonking <benonkighoma@gmail.com>
2022-12-10 02:39:15 -0800

    unigue combination of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 10):
     for j in range((num + 1), 10):
-        if (num != 8) or (j != 9):
+        if (num is not  8) or (j is not 9):
             print("{}{}, ".format(num, j), end='')
         else:
             print("{}{}".format(num, j))

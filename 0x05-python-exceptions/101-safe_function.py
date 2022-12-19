48e41a7abd68b0fc222efac0be0ed66f6f562904
Benonking <benonkighoma@gmail.com>
2022-12-19 06:31:31 -0800

    safe escecute

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except (ValueError, ZeroDivisionError, IndexError, TypeError) as i:
         stderr.write("Exception: {}".format(i))
         return None
-

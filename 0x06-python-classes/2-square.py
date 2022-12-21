1deefc6e8af46e28c28d6a6c900d85fc5ac39840
Benonking <benonkighoma@gmail.com>
2022-12-20 23:31:13 -0800

    validate private attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pythom3
+
+"""
+Module 2-square
+Defines
+"""
+
+
+class square:
+    """
+    class square definition
+    Args:
+        size(int): size of a side in square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializeses Square
+        Attributes:
+        __size(int): size of side of square, defaults to 0 if none
+        """
+        if size is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size

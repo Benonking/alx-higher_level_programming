4360f741764d87c44b37e749bd679368d081d10d
Benonking <benonkighoma@gmail.com>
2022-12-21 01:03:45 -0800

    get size - set size calcualte area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/pytthon3
+"""
+Module 4-square
+Defines class Sqaure with private attribute size
+"""
+
+
+class Square:
+    """
+    Initialises Sqaure
+    Args:
+        size (int): size of a side in sqaure
+    Funcrions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
+    """
+    def __int__(self, size=0):
+        """
+        Initialise class Square
+        Attributes:
+            sie(int): default = 0 if none
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter
+        Return size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+        Args:
+            value: sets size to value, if int and >=0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size musbe be >=0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calulate area
+        Return:
+            area
+        """
+        return (self.__size) * (self.__size)

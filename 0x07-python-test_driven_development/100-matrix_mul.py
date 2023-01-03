9df6cc738476487cab931be382411193b34259e0
Benonking <benonkighoma@gmail.com>
2023-01-03 01:34:49 -0800

    calculate matrix using numpy

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -3,6 +3,8 @@
 Module - 100-matrix_mul
 
 """
+
+
 def matrix_mul(m_a, m_b):
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
@@ -10,16 +12,16 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if len(m_a) == 0 or len(m_b) == 0 or m_a == [[]] or m_b == [[]]:
         raise ValueError("m_a can't be empty or m_b can't be empty")
-    
+
     for row in m_a:
         for i in row:
-            if not isinstance(i, (int,float)):
+            if not isinstance(i, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
         if len(row) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
         if len(row) != len(m_b[0]):
             raise ValueError("m_a and m_b can't be multiplied")
-    
+
     for row in m_b:
         for i in row:
             if not isinstance(i, (int, float)):
@@ -38,10 +40,3 @@ def matrix_mul(m_a, m_b):
             i = 0
         n_matrix.append(lists)
     return n_matrix
-
-
-
-
-  
-
-
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 101-lazy_matrix_mull
+Mubtilplies 2 matrices by using the module NumPy
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    returns matrix multiplied
+    """
+    return numpy.matmul(m_a, m_b)

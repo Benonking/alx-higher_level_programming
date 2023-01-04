81a8a1cfa3885d89911e171f5f933ac68640b853
Benonking <benonkighoma@gmail.com>
2023-01-04 01:13:23 -0800

    set anf get

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,8 +35,10 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >=0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -55,5 +57,7 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >=0")
+        else:
+            self.__height = value

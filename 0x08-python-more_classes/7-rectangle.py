319857e391b776778ea4069a818dd78ff79a40b0
Benonking <benonkighoma@gmail.com>
2023-01-05 04:20:09 -0800

    string represantation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):

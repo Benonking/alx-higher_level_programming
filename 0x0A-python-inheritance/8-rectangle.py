4c70f4e42e72abf1e4e5664f6fe511251e7dc683
Benonking <benonkighoma@gmail.com>
2023-01-09 06:56:20 -0800

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,25 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+contains sub class Rectangle that inherits BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    inherits class BaseGeometry
+    Methods
+        __init__(self,width,height)
+
+    """
+    def __init__(self, width, height):
+        """
+        Args:
+            width(int) - private
+            height(int) - private
+        """
+        self.__width = width
+        self.__height = height
+
+        super().integer_validator("width", width)
+        supper().integer_validator("height", height)

e75bde1b900e8fc7db9880fa8330fff6251daa0c
Benonking <benonkighoma@gmail.com>
2023-01-09 06:07:22 -0800

    is a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,14 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module 4-inherits_from
+
+check if object instance was inherited from a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    check if an instance of an object of a class that
+    inherited from a spefied class
+    """
+    return issubclass(obj, a_class)

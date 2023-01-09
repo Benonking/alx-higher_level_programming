c5587f8146a7923b52c05b8b56fc185babad834f
Benonking <benonkighoma@gmail.com>
2023-01-09 11:47:51 -0800

    sqaure

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,27 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module  10-square
+
+contains class Square inherits from Rectangle
+
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    inherits fromRectangle who inherits from BaseGeometry
+    methods:
+        __init__(self,size)
+    """
+    def __init__(self, size):
+        """
+        initialsize class
+        Args:
+            size(int): private
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)

3be62c560d61380387ba6f08ed94aa6a802de21a
Benonking <benonkighoma@gmail.com>
2023-01-10 07:23:02 -0800

    laod, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,10 @@ Module 7-add_item
 Contains script that adds all argu,ents to a python list and then save
 them to a file
 """
+
+
+import sys
+
 save_to_json_file = __import__('5-save_to_jason_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').save_from_json_file
 

f191bc8d22f597d39065cc7fd1cdb622c5f6db09
Benonking <benonkighoma@gmail.com>
2023-01-10 07:22:18 -0800

    laod, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 7-add_item
+Contains script that adds all argu,ents to a python list and then save
+them to a file
+"""
+save_to_json_file = __import__('5-save_to_jason_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').save_from_json_file
+
+f = add_item.json
+try:
+    a_list = load_from_json_file(f)
+except(TypeError, FileNotFoundError):
+    a_list = []
+
+for i in sys.argv[1:]:
+    a_list.append(i)
+save_to_json_file(a_list, f)

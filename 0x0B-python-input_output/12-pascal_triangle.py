59dc39714ba7704e1dfaeff254a9d33d0c04ee71
Benonking <benonkighoma@gmail.com>
2023-01-11 12:48:32 -0800

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 12--pascal_triangle
+contains a function that impliments list of pascals triangle given size
+"""
+
+from math import factorial
+
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+
+    items = [[i]]
+    for rows in range(n-1):
+        items.append(a+b for a, b in zip([0] + items[-1], items[-1] + [0]))
+    return items

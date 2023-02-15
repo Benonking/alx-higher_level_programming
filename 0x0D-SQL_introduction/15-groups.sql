07b7602859e20ded590ac8e8b0d88b2f870c02ed
Benon <benonkighoma@gmail.com>
2023-02-15 14:18:49 +0300

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records witht the same score
-SELECT score COUNT(score) as 'number' FROM second_table GROUP BY score ORDER BY score ORDER BY number DESC;
+SELECT score, COUNT(score) as 'number' FROM second_table GROUP BY score ORDER BY number DESC;

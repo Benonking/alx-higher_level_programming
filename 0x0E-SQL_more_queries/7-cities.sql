437aa4560c7ecd428bce7a4de74701a9b75c9155
Benon <benonkighoma@gmail.com>
2023-02-16 18:41:29 +0300

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,11 +4,16 @@
 --     state_id INT  cant be null foerign key)
 -- script shoul not fail if db or table doesnt exist
 
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+USE hbtn_0d_usa;
+
+/* create cities table */
+CREATE TABLE IF NOT EXISTS cities (
+	   PRIMARY KEY(id),
+	   id		INT UNIQUE AUTO_INCREMENT NOT NULL,
+	   state_id INT 	   				  NOT NULL,
+	   name 	VARCHAR(256),
+	   FOREIGN KEY (state_id) REFERENCES states(id)
 );
 	

351d99647adf57e652879335711c12041ec5f7ef
Benon <benonkighoma@gmail.com>
2023-02-16 18:32:20 +0300

    states table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,7 +4,10 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
+
+/*create table*/
 CREATE TABLE IF NOT EXISTS states (
-       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       PRIMARY KEY(id),
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
        name VARCHAR(256) NOT NULL
 );

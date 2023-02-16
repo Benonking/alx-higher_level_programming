145ef5e3c633b0efe93ed5decf756ef8d9f17d74
Benon <benonkighoma@gmail.com>
2023-02-16 18:39:27 +0300

    cities of califonia

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- list all cities that can be found in the db
-SELECT id, name 
-FROM cities 
-WHERE state_id = (SELECT id FROM states WHERE name = "califonia") GROUP BY id ASC; 
+SELECT c.id, c.name
+	  FROM cities AS c, states AS s
+WHERE c.state_id=s.id AND s.name = 'California'
+	  ORDER BY c.id ASC;

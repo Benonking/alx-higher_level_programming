3f8e7418876add0a2e9908931ffccdb4dc2f601c
Benon <benonkighoma@gmail.com>
2023-02-16 16:48:08 +0300

    create db , add user, give privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- creates database hbtn_0d_2 and user -> user_0d_2
+-- user has only select privilege in db
+-- user password -> user_0d_2_pwd
+-- isdb already exists script should not fail
+-- if user already exits script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXIISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
+

5f8fdfb2bc423cc411976742b3bd988b80f57eab
Benon <benonkighoma@gmail.com>
2023-02-16 21:08:44 +0300

    left join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- list all shows contsained in db
+--- display tv_show.title, tv_show_genres.genre_id
+-- if a show doesnt have a genre display null
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC

7eb406c439acdb14b65c29e7c2927eb00a851e52
Benon <benonkighoma@gmail.com>
2023-02-16 16:35:18 +0300

    create and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- create user user_0d_1
+-- should have all privileges
+-- set passeord to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

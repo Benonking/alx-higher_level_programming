79715eea979a8d4a6c59944866cc0fbc84c8a4dd
Benon <benonkighoma@gmail.com>
2023-02-21 16:54:35 +0300

    create db and table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,12 @@
--- create hbtn_0d_usa and table states(
---id INT unique,auto generated cant be NULL PK
--- name VARCHAR(256)
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- description of table data:
+--      id INT unique, auto generated, can’t be null and is a primary key
+--      name VARCHAR(256), can't be null
+-- should not fail if either exists
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-
-/* creating table */
 CREATE TABLE IF NOT EXISTS states (
-	   id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	   name VARCHAR(256) NOT NULL
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
 );

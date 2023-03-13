5ac84ae797f5b3cf1f84a73882cff2a7a01af7b3
Benon <benonkighoma@gmail.com>
2023-03-13 17:10:45 +0300

    loop languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScipt is amazing'];
-for (elem of arr){
-	console.log(elem);
+arr.forEach((element)=>{
+	console.log(element);
 }
+);

cf0aa5e3e484d4a455c4baaecd1c6bb0bd935276
Benon <benonkighoma@gmail.com>
2023-03-13 19:48:50 +0300

    fix indent

--- 0x12-javascript-warm_up/8-sqaure.js
+++ 0x12-javascript-warm_up/8-sqaure.js
@@ -2,14 +2,14 @@
 const size  = Math.floor(Number(process.argv[2]));
 
 if (isNaN(size)){
-	console.log('Missing size');
+  console.log('Missing size');
 }
 else{
-	for (let i = 1; i <= size;i++)
-	{
-		let row = '';
-		for (let x = 1; x <= size; x++) row += 'X';
-		console.log(row);
-	}
+  for (let i = 1; i <= size;i++)
+    {
+      let row = '';
+      for (let x = 1; x <= size; x++) row += 'X';
+        console.log(row);
+    }
 }
 

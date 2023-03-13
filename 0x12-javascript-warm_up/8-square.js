8e0099348c17302c96657536a4617409a5e18db5
Benon <benonkighoma@gmail.com>
2023-03-13 19:52:48 +0300

    fix indent

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(size)){
   console.log('Missing size');
 }
 else{
-  for (let i = 1; i <= size;i++)
+  for (let i = 1; i <= size; i++)
     {
       let row = '';
       for (let x = 1; x <= size; x++) row += 'X';

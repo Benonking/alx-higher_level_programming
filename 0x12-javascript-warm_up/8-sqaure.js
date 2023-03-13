4e2dbc540fa5e3bb155bf086b76cb0019e03b86f
Benon <benonkighoma@gmail.com>
2023-03-13 18:35:00 +0300

    square

--- 0x12-javascript-warm_up/8-sqaure.js
+++ 0x12-javascript-warm_up/8-sqaure.js
@@ -5,11 +5,11 @@ if (isNaN(size)){
 	console.log('Missing size');
 }
 else{
-	for (let i = 0; i <= size;i++)
+	for (let i = 1; i <= size;i++)
 	{
 		let row = '';
-		for (let x = 0; x <= size; x++) row += 'X';
-		console.log(c);
+		for (let x = 1; x <= size; x++) row += 'X';
+		console.log(row);
 	}
 }
 

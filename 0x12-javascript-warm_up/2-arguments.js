0f70111ace53e3060285c124eb34fa2499acd6bb
Benon <benonkighoma@gmail.com>
2023-03-13 16:06:58 +0300

    command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 const index = process.argv.length;
-if (index <= 2)
-{
-	console.log("No argument");
-}
-else if (index == 3)
-{
-	console.log('Argument found');
-}
-else{
-	console.log('Arguments found');
-}
+console.log(index === 2 ? 'No argument' : index === 3 ? 'Argument found' : 'Arguments found');

14ad217341d7c71ec25fc3f936a06551d0d7ca5e
Benon <benonkighoma@gmail.com>
2023-03-13 17:27:17 +0300

    loop c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const num = Math.floor(Number(process.argv[2]));
+
+if(isNaN(num)){
+console.log('Missing number of occurencies');
+}
+else{
+	for (let i = 0; i<= num; i++){
+		console.log('C is fun');
+	}
+}
+

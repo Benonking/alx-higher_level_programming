f6c4e355d604b510a624edc7c47ef5573f0e1aee
Benon <benonkighoma@gmail.com>
2023-03-14 09:57:41 +0300

    fixt semistandard style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const argv = process.argv;
 const size = parseInt(argv[2]);
-const character= 'X'
-if (isNaN(size)){
+const character = 'X';
+if (isNaN(size)) {
   console.log('Missing size');
-}
-else{
-  for (let i = 1; i <= size; i++){
+} else {
+  for (let i = 1; i <= size; i++) {
     console.log(character.repeat(size));
   }
 }
-

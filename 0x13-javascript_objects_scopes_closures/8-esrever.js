aa16c8def83757cb7cf4f278bf24cec63f61fa08
Benon <benonkighoma@gmail.com>
2023-03-15 18:06:00 +0300

    reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
-};
+  let idx = 0;
+  let length = list.length - 1;
+  while(idx < length){
+    let temp = list[idx];
+    list[idx] = list[length];
+    list[length] = temp;
+    idx ++;
+    length--;
+  }
+  return list;
+}

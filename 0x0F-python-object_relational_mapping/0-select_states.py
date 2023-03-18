76690fd8b8c2a6e24d625ba1ac45b19f141df978
Benonking <benonkighoma@gmail.com>
2023-03-18 13:03:14 +0300

    list all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 import MySQLdb as sql
 from sys import argv
 if (__name__ == '__main__'):
-    conn = sql.connect(host = 'localhost', port=3306, user=argv[1],
+    conn = sql.connect(host='localhost', port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states")
@@ -10,4 +10,4 @@ if (__name__ == '__main__'):
     for row in query:
         print(row)
     cur.close()
-    conn.close() 
+    conn.close()

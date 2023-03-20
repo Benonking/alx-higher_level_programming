586dc332866aed70436fa9a7f86d8c2a8c9e75b7
Benonking <benonkighoma@gmail.com>
2023-03-20 17:44:48 +0300

    contain a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,10 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    
     for inst in session.query(State).filter(State.name.like('%a%')):
         print(inst.id, inst.name, sep=": ")

6b6b4cb46b3f183a70db36022cfc6c8eed16c3e3
Benonking <benonkighoma@gmail.com>
2023-03-22 17:08:23 +0300

    delete all with 'a'

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,11 +11,10 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    inst = session.query(State).filter_by(id=2).first()
-    inst.name = 'New Mexico'
     session.commit()
     for inst in session.query(State).filter(State.name.like('%a%')):
         session.delete(inst)

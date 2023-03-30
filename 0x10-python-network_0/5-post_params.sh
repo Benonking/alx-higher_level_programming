df0536aaefa8c2fb2db809ff7f0fa540e0e672db
Benonking <benonkighoma@gmail.com>
2023-03-30 17:16:42 +0300

    correct post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends POST request, to URL, displays the body of response
 # variable email=test@gmail.com, subject= I will always be here for PLD
-curl -sd "email=test@gmail.com&subject=I will always be here for PLD" -X POST $1
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"

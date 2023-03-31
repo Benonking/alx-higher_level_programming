021b481ac0fd47a0f8a43aeb08a6823654e4c563
Benonking <benonkighoma@gmail.com>
2023-03-31 16:52:22 +0300

    header id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 """
+Take in URL, sends request and dispys the value of the X-request-Id
+    variable found in the header of the resposne
 """
+from sys import argv
+from urllib import argv
+
+
+if __name__ == "__name__":
+    with request.urlopen(argv[1]) as resp:
+        print(resp.headers.get("X-Request-Id"))

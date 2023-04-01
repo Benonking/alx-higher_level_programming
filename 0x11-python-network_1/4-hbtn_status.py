0a9b1f79f07d58b34a78daba3782c92e7bf4d722
Benonking <benonkighoma@gmail.com>
2023-04-01 10:40:27 +0300

    status check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ fetch https://alx-intranet.hbtn.io/status
 import requests
 
 if __name__ == '__main__':
-    r = requests.get('https://https://alx-intranet.hbtn.io/stattus')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     response = r.text
     print("Body response:")
     print("\t- type: {}".format(type(response)))

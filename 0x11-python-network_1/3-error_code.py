3360396be691aabb69db53ecc0bde9c12a80502e
Benonking <benonkighoma@gmail.com>
2023-04-01 09:35:24 +0300

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     try:
         with request.urlopen(argv[1]) as resp:
             rep = resp.read()
-            print(rep.decode('utf8'))
+            print(rep.decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))

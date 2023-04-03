abaa43e93f511827c388a381dcb12fac3ceec187
Benonking <benonkighoma@gmail.com>
2023-04-03 09:54:38 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,14 +5,14 @@ Take in email as argument send POST request to it
 """
 
 from sys import argv
-import urllib.parse as parse
-import urllib.request as request
+import urllib.parse
+import urllib.request
 
 if __name__ == '__main__':
     url = argv[1]
     value = {'email': argv[2]}
-    data = parse.urlencode(value).encode('utf-8')
-    req = request.Request(url, data)
-    with request.urlopen(req) as resp:
-        r = resp.read()
-        print(r.read().decode('utf-8'))
+    data = urllib.parse.urlencode(value).encode('ascii')
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as resp:
+        print(resp.read().decode('utf-8'))

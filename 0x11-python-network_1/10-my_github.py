2a928d4c14b402bc0317998786a522b76574850f
Benonking <benonkighoma@gmail.com>
2023-04-03 09:33:24 +0300

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Takes in ur githhub credentials(Username and password)
+    and uses the GITHUB API to access ur id
+"""
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], argv[2])
+    r =  requests.get('https:/apt.github.com/user', auth=auth)
+    print(r.json().get('id'))

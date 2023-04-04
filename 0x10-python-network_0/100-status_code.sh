ffb9d96d055f8cceb738a26610fb75944e5f6949
Benonking <benonkighoma@gmail.com>
2023-04-04 08:58:11 +0300

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # send request to a URL passed as an argument 
-#	and display status code of the response
-curl -sI -w '%{http_code}' "$1" -o /dev/null
+curl -sI -w '%{response_code}' "$1" -o /dev/null

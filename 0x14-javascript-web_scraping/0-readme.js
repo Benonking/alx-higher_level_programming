f1ce2317892fcfc38c3a218c0b47d8dc911150f7
Benonking <benonkighoma@gmail.com>
2023-05-23 16:07:09 +0300

    fix semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const fileName = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(fileName, 'utf-8', (err, data) => {
-    if (err){
-        console.log(err);
-    }else{
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
-    }
+  }
 });

b2bc5e138af32479f4eb5e2d2bf0bf635a61199d
Benonking <benonkighoma@gmail.com>
2023-05-23 15:16:31 +0300

    write fo a file

---
+++ 0x14-javascript-web_scrapping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileName = process.argv[2];
+const text = process.argv[3];
+fs.writeFile(fileName, text, 'utf-8', (err, data) => {
+  if (err) throw err;
+});

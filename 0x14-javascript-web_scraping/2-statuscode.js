c9e3eca3e56a0fdf02c5b9607f88e9481fac515d
Benonking <benonkighoma@gmail.com>
2023-05-23 16:36:01 +0300

    get status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let url = process.argv[2];
-let response = fetch(req);
-request(url, function (error, response, body){
-    if (error) throw error;
-    console.log('code: ', response.statusCode);
-});
+const url = process.argv[2];
+request
+  .get(url).on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+  });

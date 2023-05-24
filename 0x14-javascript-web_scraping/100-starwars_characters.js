254f8380d965aa44e0dc277a5921ea51363f7801
Benonking <benonkighoma@gmail.com>
2023-05-24 14:57:22 +0300

    print characters from star war movies

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// print all characters of a starwars movie
+const request = require('request');
+const ID = process.argv[2];
+const url = 'http://swapi.co/api/films/' + ID;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const charList = JSON.parse(body).characters;
+    for (let j = 0; j < charList.length; j++) {
+      request(charList[j], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
